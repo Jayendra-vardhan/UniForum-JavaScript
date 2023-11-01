b98ad09789f28d483c1675c241116a9e4793f774
jayendarvardhan <jayendravardhan@gmail.com>
2023-11-02 00:33:59 +0530

    Uniforum

---
+++ client/src/components/common/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Jumotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-3">UniForum</h1>
+      <p class="lead">Fostering Collaborative Discussions and Knowledge Exchange</p>
+      <hr class="my-4" />
+      <p className="lead">A disscussion forum for interactive people.</p>
+    </div>
+  );
+};
+
+export default Jumotron;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();

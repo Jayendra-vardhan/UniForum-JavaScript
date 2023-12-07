ae62faa96f1a1050a98b086e6a6d39bf396ae5b4
Jayendra Vardhan Singh <70995151+jayendra-587@users.noreply.github.com>
2023-12-07 16:01:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,13 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://uniforum.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("request successfully sent!");

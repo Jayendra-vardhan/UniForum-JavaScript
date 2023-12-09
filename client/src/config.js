a3bfa7eea76a0d6f16a58ebdf99c83886fef389e
jayendarvardhan <jayendravardhan@gmail.com>
2023-12-09 09:27:07 +0530

    replies

--- client/src/config.js
+++ client/src/config.js
@@ -2,5 +2,5 @@ export const api = {
   tagsEndPoint: "https://uniforum-master-server.vercel.app/tags/",
   usersEndPoint: "https://uniforum-master-server.vercel.app/users/",
   postsEndPoint: "https://uniforum-master-server.vercel.app/posts/",
-  repliesEndPoint: "https://uniforum-master-server.vercel.app/reply/",
+  repliesEndPoint: "https://uniforum-master-server.vercel.app/replies/",
 };

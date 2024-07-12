ed6b4ba4ea3c8765349d8f2845b52c1f5878a1cc
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-07-13 02:19:11 +0530

    updated dockerfile

--- server/index.js
+++ server/index.js
@@ -26,15 +26,14 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "https://uniforum-master-client.vercel.app",
-      "http://localhost:3000"
-    ],
-    methods: ["POST", "GET", "PUT","DELETE","UPDATE"],
-    credentials: true,
-  })
+app.use(cors({
+                origin: [
+                  "https://uniforum-master-client.vercel.app",
+                  "http://localhost:3000"
+                ],
+                methods: ["POST", "GET", "PUT","DELETE","UPDATE"],
+                credentials: true,
+              })
 );
 
 app.get("/", (req, res) => {

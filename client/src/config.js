71a35a26bd2b4d72b28911e9c7ad92e5beb3de2e
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-08-06 01:44:22 +0530

    dockerfiles updated and configures

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,20 @@
+// export const api = {
+//   tagsEndPoint: "https://uniforum-master-server.vercel.app/tags/",
+//   usersEndPoint: "https://uniforum-master-server.vercel.app/users/",
+//   postsEndPoint: "https://uniforum-master-server.vercel.app/posts/",
+//   repliesEndPoint: "https://uniforum-master-server.vercel.app/replies/",
+// };
+
 export const api = {
-  tagsEndPoint: "https://uniforum-master-server.vercel.app/tags/",
-  usersEndPoint: "https://uniforum-master-server.vercel.app/users/",
-  postsEndPoint: "https://uniforum-master-server.vercel.app/posts/",
-  repliesEndPoint: "https://uniforum-master-server.vercel.app/replies/",
+  tagsEndPoint: "http://server:5000/tags/",
+  usersEndPoint: "http://server:5000/users/",
+  postsEndPoint: "http://server:5000/posts/",
+  repliesEndPoint: "http://server:5000/replies/",
 };
+
+// export const api = {
+//   tagsEndPoint: "http://localhost:5000/tags/",
+//   usersEndPoint: "http://localhost:5000/users/",
+//   postsEndPoint: "http://localhost:5000/posts/",
+//   repliesEndPoint: "http://localhost:5000/replies/",
+// };

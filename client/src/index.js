2ea8315d08cbe62c21ba402a88d05ae77e1a6402
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-08-06 03:13:11 +0530

    clients index.js updated

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
